740b7e90326e21f3521a5303140d37f368c07578
Mogira <@mogira_jubeat>
2017-03-15 01:02:16 +0000

    Change the way to add/remove notification observers

--- NotificationController.py
+++ NotificationController.py
@@ -16,74 +16,38 @@ class NotificationController:
 			pass
 		def MP4PWillClose(_self, _cmd):
 			self.removeAllObservers()
-		self.nmo = create_objc_class(
+		self.method_table = {
+			'UIApplicationWillResignActiveNotification'
+				: willResignActive,
+			'UIApplicationDidBecomeActiveNotification'
+				: didBecomeActive,
+			'MPMusicPlayerControllerPlaybackStateDidChangeNotification'
+				: playbackStateDidChange,
+			'MPMusicPlayerControllerNowPlayingItemDidChangeNotification'
+				: nowPlayingItemDidChange,
+			'MP4PythonistaWillCloseNotification'
+				: MP4PWillClose,
+		}
+		self.mp4p_nc = create_objc_class(
 				'MP4PNotificationController',
-				methods = [
-					willResignActive,
-					didBecomeActive,
-					playbackStateDidChange,
-					nowPlayingItemDidChange,
-					MP4PWillClose
-				]
-			).alloc()
-		self.nc = ObjCClass('NSNotificationCenter').defaultCenter()
+				methods = self.method_table.values()
+		).alloc()
+		self.ndc = ObjCClass('NSNotificationCenter').defaultCenter()
 	def registerAllObservers(self):
-		self.nc.addObserver_selector_name_object_(
-			self.nmo,
-			sel('willResignActive'),
-			'UIApplicationWillResignActiveNotification',
-			None
-		)
-		self.nc.addObserver_selector_name_object_(
-			self.nmo,
-			sel('didBecomeActive'),
-			'UIApplicationDidBecomeActiveNotification',
-			None
-		)
-		self.nc.addObserver_selector_name_object_(
-			self.nmo,
-			sel('playbackStateDidChange'),
-			'MPMusicPlayerControllerPlaybackStateDidChangeNotification',
-			None
-		)
-		self.nc.addObserver_selector_name_object_(
-			self.nmo,
-			sel('nowPlayingItemDidChange'),
-			'MPMusicPlayerControllerNowPlayingItemDidChangeNotification',
-			None
-		)
-		self.nc.addObserver_selector_name_object_(
-			self.nmo,
-			sel('MP4PWillClose'),
-			'MP4PythonistaWillCloseNotification',
-			None
-		)
+		for k,v in self.method_table.items():
+			self.ndc.addObserver_selector_name_object_(
+				self.mp4p_nc,
+				sel(v.__name__),
+				k,
+				None
+			)
 		self.mpc.player.beginGeneratingPlaybackNotifications()
 	def removeAllObservers(self):
-		self.nc.removeObserver_name_object_(
-			self.nmo,
-			'UIApplicationWillResignActiveNotification',
-			None
-		)
-		self.nc.removeObserver_name_object_(
-			self.nmo,
-			'UIApplicationDidBecomeActiveNotification',
-			None
-		)
-		self.nc.removeObserver_name_object_(
-			self.nmo,
-			'MPMusicPlayerControllerPlaybackStateDidChangeNotification',
-			None
-		)
-		self.nc.removeObserver_name_object_(
-			self.nmo,
-			'MPMusicPlayerControllerNowPlayingItemDidChangeNotification',
-			None
-		)
-		self.nc.removeObserver_name_object_(
-			self.nmo,
-			'MP4PythonistaWillCloseNotification',
-			None
-		)
+		for k in self.method_table.keys():
+			self.ndc.removeObserver_name_object_(
+				self.mp4p_nc,
+				k,
+				None
+			)
 		self.mpc.player.endGeneratingPlaybackNotifications()
 
